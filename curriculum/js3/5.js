2fe9dcfacf6874a7ee5a94c6c159133cbde0b660
Alberto Lopez <alopez@garagescript.org>
2018-02-01 18:58:33 -0800

    map

---
+++ curriculum/js3/5.js
@@ -0,0 +1,14 @@
+/*
+ * map - takes in an object and a function and calls the function with each key, value.
+ * @param {object} a
+ * @param {function} b
+ * @return {nothing}
+ **/
+
+const solution = (a, b)=>{
+  return 0;
+}
+module.exports = {
+  solution
+}
+
